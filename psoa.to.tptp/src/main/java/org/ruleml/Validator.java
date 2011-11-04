15c916e417d4ade777bcbf5e76d8686406f5d395
reuben.peterpaul@gmail.com <reuben.peterpaul@gmail.com@6492688d-5cc4-82a0-8038-e7a1b1ca8359>
2011-11-04 21:36:34 +0000

    Merged Sadnan and Alex's code into our group project.
    git-svn-id: http://psoa-to-tptp.googlecode.com/svn/trunk@19 6492688d-5cc4-82a0-8038-e7a1b1ca8359

---
+++ psoa.to.tptp/src/main/java/org/ruleml/Validator.java
@@ -0,0 +1,149 @@
+
+package org.ruleml;
+
+
+import java.io.*;
+
+import java.util.*;
+
+import gnu.getopt.LongOpt;
+import gnu.getopt.Getopt;    
+
+import org.ruleml.api.presentation_syntax_parser.*;
+import org.ruleml.api.*;
+
+import org.antlr.runtime.*;
+import org.antlr.runtime.tree.*;
+
+
+/** Command line utility to validate the syntax of PSOA RuleML files. */
+public class Validator {
+
+
+    public static void main(String[] args) 
+    {
+	boolean importClosure = false;
+	String[] ruleBaseFileNames = null;
+
+	
+	LongOpt[] longOpts = new LongOpt[256];
+	
+	// Reserved short option names: i ?
+    	longOpts[0] = new LongOpt("help", LongOpt.NO_ARGUMENT, null, '?');
+	longOpts[1] = new LongOpt("import_closure", LongOpt.NO_ARGUMENT, null, 'i');
+
+	Getopt optionsParse = new Getopt("", args, "?i", longOpts);
+	
+	for (int opt = optionsParse.getopt();
+	     opt != -1;
+	     opt = optionsParse.getopt())
+	    {
+		switch (opt)
+		    {
+		    case '?': 
+			printUsage();
+			System.exit(1);
+
+		    case 'i':
+			importClosure = true;
+			break;
+
+		    default:
+			assert false;
+
+		    }; // switch (opt)
+
+
+	    }; // for (int nextOpt = optionsParse.getopt();
+	
+	int optInd = optionsParse.getOptind();
+
+	if (args.length > optInd)
+	    {
+		ruleBaseFileNames = new String[args.length - optInd];
+		for (int i = optInd; i < args.length; ++i)
+		    ruleBaseFileNames[i - optInd] = args[i];
+	    }
+	else
+	    {
+		System.out.println("No rule base file specified.");
+		printUsage();
+		System.exit(1);
+	    };
+	
+	try 
+	    {	
+		DefaultAbstractSyntax absSynFactory = 
+		    new DefaultAbstractSyntax();
+
+	       
+		for (int i = 0; i < ruleBaseFileNames.length; ++i)
+		    {
+			System.err.println("\n\n% Processing rule base " + ruleBaseFileNames[i]);
+			File ruleBaseFile = new File(ruleBaseFileNames[i]);
+			
+			Reader reader = new FileReader(ruleBaseFile);
+
+			ANTLRReaderStream stream = 
+			    new ANTLRReaderStream(reader);
+
+			RuleMLPresentationSyntaxLexer lexer = 
+			    new RuleMLPresentationSyntaxLexer(stream);
+
+			CommonTokenStream tokenStream = 
+			    new CommonTokenStream(lexer);
+
+			RuleMLPresentationSyntaxParser parser = 
+			    new RuleMLPresentationSyntaxParser(tokenStream);
+
+// 			AbstractSyntax.Document doc = 
+// 			    parser.parse(ruleBaseFile,absSynFactory);
+			
+			 RuleMLPresentationSyntaxParser.top_level_item_return kb = 
+			    parser.top_level_item();
+
+			reader.close();
+			
+			CommonTree ast = (CommonTree)kb.getTree();
+
+			RuleMLPresentationASTGrammar treeParser = 
+			    new RuleMLPresentationASTGrammar(new CommonTreeNodeStream(ast));
+			
+			AbstractSyntax.Document doc = 
+			    treeParser.document(absSynFactory);
+
+			System.out.println("AST: " + ast);
+			for (Object child : ast.getChildren())
+			    System.out.println("CHILD: " + child);
+			
+			//System.out.println(doc.toStringInPresSyntax("   "));
+			
+
+			System.err.println("% End of rule base " + ruleBaseFileNames[i]);
+
+		    }; // for (int i = 0; i < ruleBaseFileNames.length; ++i)
+
+	    }
+	catch (Exception ex)
+	    {
+		System.out.println("Error: " + ex);
+		ex.printStackTrace();
+		System.exit(1);
+	    };
+
+
+
+    } // main(String[] args)
+
+
+    private static void printUsage() {
+
+	System.out.println("Usage: org.ruleml.Validator [OPTIONS] <rule base file>+");
+	System.out.println("Options:");
+	System.out.println("\t--help -? \n\t\t Print this message.");
+	System.out.println("\t--import_closure -i \n\t\tProcess the whole import closures of the rule bases.");
+    } // printUsage() 
+
+
+
+} // class Validator
