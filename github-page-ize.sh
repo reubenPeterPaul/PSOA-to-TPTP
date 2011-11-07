43dcabb86925bb52b49e6cde7c9565bdf03d3b50
Reuben Peter-Paul <reuben.peterpaul@gmail.com>
2011-11-07 11:26:32 -0400

    Fixed git script

--- github-page-ize.sh
+++ github-page-ize.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 BASE_DIR=$(pwd)
-HTML_DIR="$BASE_DIR/_build/html"
-CS6795PROJ="$HTML_DIR/project-proposal"
+HTML_DIR="$BASE_DIR/build/html"
+CS6795PROP="$HTML_DIR/project-proposal"
 
 cd $HTML_DIR
 mv _sources sources
 mv _static static
 mv _images images
 sed -i 's/_static/static/g' *.html 
+sed -i 's/_images/images/g' *.html  #TODO: make procedure for these sed commands
 sed -i 's/_sources/sources/g' *.html
 mv main.html index.html
 
+cd $CS6795PROP
+mv _sources sources
+mv _static static
+mv _images images
+sed -i 's/_static/static/g' *.html 
+sed -i 's/_images/images/g' *.html  #TODO: make procedure for these sed commands
+sed -i 's/_sources/sources/g' *.html
