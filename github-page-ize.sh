c51db0036aeacf528a53da32f38f92b3c2aa1a3e
Reuben Peter-Paul <reuben.peterpaul@gmail.com>
2011-12-09 11:52:22 -0400

    Fixing stylesheet resolution.

--- github-page-ize.sh
+++ github-page-ize.sh
@@ -3,19 +3,11 @@ BASE_DIR=$(pwd)
 HTML_DIR="$BASE_DIR/build/html"
 CS6795PROP="$HTML_DIR/project-proposal"
 
+echo "cd $HTML_DIR"
 cd $HTML_DIR
-mv _sources sources
-mv _static static
-mv _images images
+mv _sources sources && mv _static static && mv _images images
 sed -i 's/_static/static/g' *.html 
 sed -i 's/_images/images/g' *.html  #TODO: make procedure for these sed commands
 sed -i 's/_sources/sources/g' *.html
 mv main.html index.html
 
-cd $CS6795PROP
-mv _sources sources
-mv _static static
-mv _images images
-sed -i 's/_static/static/g' *.html 
-sed -i 's/_images/images/g' *.html  #TODO: make procedure for these sed commands
-sed -i 's/_sources/sources/g' *.html
