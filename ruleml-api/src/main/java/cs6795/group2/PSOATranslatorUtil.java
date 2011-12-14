a7532e131d5d1cea981b8cfb0d5fdbce822caad4
zougen04@gmail.com <zougen04@gmail.com@6492688d-5cc4-82a0-8038-e7a1b1ca8359>
2011-12-14 22:06:34 +0000

    git-svn-id: http://psoa-to-tptp.googlecode.com/svn/trunk@81 6492688d-5cc4-82a0-8038-e7a1b1ca8359

--- ruleml-api/src/main/java/cs6795/group2/PSOATranslatorUtil.java
+++ ruleml-api/src/main/java/cs6795/group2/PSOATranslatorUtil.java
@@ -3,40 +3,58 @@ package cs6795.group2;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class PSOATranslatorUtil {
 	public static final String Hash = "#";
 	public static final String Period = ".";
 	public static final String Comma = ", ";
 	public static final String Amp = " & ";
+	public static final String ImpliedBy = " <= ";
 	public static final String Member = "Member";
 	public static final String TupTerm = "Tupterm";
 	public static final String SlotTerm = "Sloterm";
 	public static final String Hypothesis = "hypothesis";
 	public static final String Axiom = "axiom";
 	public static final String FOF = "fof";
-	
+
 	public static boolean isNull(Object o) 
 	{ 
 		return o==null;
 	}
-	
+
 	public static int counter = 1;
 	public static String randomOID() {
 		return "int:oid"+counter++;
 	}
-	
-	public static String psoaTerm(String type, List<String> tuples, List<String> slots) {
-		return psoaTerm(randomOID(),type, tuples,slots);
+
+	public static String getVarName(String var) {
+		char c = var.charAt(1);
+		if (Character.isUpperCase(c)) {
+			return var.substring(1);
+		}
+		
+		return String.valueOf(Character.toUpperCase(c)).concat(var.substring(2));
 	}
 	
-	public static String psoaTerm(String oid, String type, List<String> tuples, List<String> slots) {
+	public static String getConstName(String con) {
+		char c = con.charAt(0);
+		if (Character.isLowerCase(c))
+			return con;
+		
+		return String.valueOf(Character.toLowerCase(con.charAt(0))).concat(con.substring(1));
+	}
+	
+	public static StringBuilder psoaStr(String type, List<String> tuples, List<String> slots) {
+		return psoaStr(randomOID(), type, tuples, slots);
+	}
+	
+	public static StringBuilder psoaStr(String oid, String type, List<String> tuples, List<String> slots) {
 		if (oid == null) {
 			oid = randomOID();
 		}
+
 		StringBuilder sb = member(oid,type);
 		if (tuples.isEmpty() && slots.isEmpty()) {
-			return sb.toString();
+			return sb;
 		}
 		
 		for(String s : tuples) {
@@ -46,24 +64,22 @@ public class PSOATranslatorUtil {
 		for (String t : slots) {
 			sb.append(amp(slot(oid, t)));
 		}
-		return sb.toString();
+		
+		return sb;
 	}
 	
-	public static StringBuilder parenthesize(StringBuilder b) {
-		StringBuilder c = builder("(");
-		c.append(b);
-		c.append(")");
-		return c;
+	public static StringBuilder parenthesize(StringBuilder b) {		
+		return b.insert(0, "(").append(")");
 	}
-	
+
 	public static StringBuilder fofSentence(StringBuilder b) {
-		return builder(FOF).append(parenthesize(b)).append(Period);
+		return parenthesize(b).insert(0, FOF).append(Period);
 	}
 	
 	public static StringBuilder member(String oid, String type) {
 		return builder(Member).append(parenthesize(builder(oid).append(Comma).append(type)));
 	}
-	
+
 	public static StringBuilder tuple(String oid, String term) {
 		return builder(TupTerm).append(parenthesize(builder(oid).append(Comma).append(term)));
 	}
@@ -73,7 +89,15 @@ public class PSOATranslatorUtil {
 	}
 	
 	public static StringBuilder amp(StringBuilder b) {
-		return new StringBuilder(Amp).append(b);
+		return b.insert(0, Amp);
+	}
+
+	public static StringBuilder existStr(StringBuilder varList, StringBuilder clause) {
+		return varList.insert(0, "?[").append(']').append(parenthesize(clause));
+	}
+	
+	public static StringBuilder allStr(StringBuilder b) {
+		return b.insert(0, "![").append(']');
 	}
 	
 	public static void collectTerm(StringBuilder b, String term) {
@@ -82,7 +106,6 @@ public class PSOATranslatorUtil {
 		} else {
 			b.append(term);
 		}
-		
 	}
 	
 	public static void collectTerms(StringBuilder b, String...strings) {
@@ -91,26 +114,30 @@ public class PSOATranslatorUtil {
 		}
 	}
 	
-	public static StringBuilder hypothesis(StringBuilder b) {
-		return parenthesize(builder(randomFactId()).append(Comma).append(Hypothesis).append(Comma).append(b));
+	public static StringBuilder foffactStr(StringBuilder b) {
+		return fofSentence(builder(randomFactId()).append(Comma + Hypothesis + Comma).append(b));
 	}
 	
-	public static StringBuilder axiom(StringBuilder b) {
-		return parenthesize(builder(randomRuleId()).append(Comma).append(Axiom).append(Comma).append(b));
+	public static StringBuilder fofruleStr(StringBuilder b) {
+		return fofSentence(builder(randomRuleId()).append(Comma + Axiom + Comma).append(b));
+	}
+
+	public static StringBuilder ruleStr(StringBuilder head, StringBuilder body) {
+		return parenthesize(head).append(ImpliedBy).append(parenthesize(body));
 	}
 	
 	public static int factCounter = 1;
 	public static String randomFactId() {
-		return "fact"+factCounter++;
+		return "fact" + factCounter++;
 	}
 	
 	public static int ruleCounter = 1;
 	public static String randomRuleId() {
-		return "rule"+ruleCounter++;
+		return "rule" + ruleCounter++;
 	}
 	
 	public static StringBuilder builder() { return builder(""); }
-	public static StringBuilder builder(String s) { return new StringBuilder(s); }
+	public static StringBuilder builder(String s) { return new StringBuilder(200).append(s); }
 	
 	public static <T> List<T> list() {
 		return new ArrayList<T>();
